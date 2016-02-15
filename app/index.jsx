a508de661ee63ec67bda90968c88bbaf2d3dcd0f
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2016-02-15 18:03:05 +0700

    crossrider fail load

--- app/index.jsx
+++ app/index.jsx
@@ -2,16 +2,16 @@ require('expose?$!expose?jQuery!jquery');
 //require('expose?$!expose?jQuery!./libs/crossriderAPI.js');
 require("bootstrap-webpack");
 
-require('expose?$!./libs/crossrider/platformVersion.js');
-require('expose?$!./libs/crossrider/consts.js');
-require('expose?$!./libs/crossrider/logging.js');
-require('expose?$!./libs/crossrider/reports.js');
-require('expose?$!./libs/crossrider/xhr.js');
-require('expose?$!./libs/crossrider/cookie.js');
-require('expose?$!./libs/crossrider/message.js');
-require('expose?$!./libs/crossrider/pageAction.js');
-require('expose?$!./libs/crossrider/installer.js');
-require('expose?$!./libs/crossrider/app_api.js');
+require('script!./libs/crossrider/platformVersion.js');
+require('script!./libs/crossrider/consts.js');
+require('script!./libs/crossrider/logging.js');
+require('script!./libs/crossrider/reports.js');
+require('script!./libs/crossrider/xhr.js');
+require('script!./libs/crossrider/cookie.js');
+require('script!./libs/crossrider/message.js');
+require('script!./libs/crossrider/pageAction.js');
+require('script!./libs/crossrider/installer.js');
+require('script!./libs/crossrider/app_api.js');
 
 
 import './main.css';
